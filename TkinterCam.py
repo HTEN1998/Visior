cc6c4d1fb5c3eb996bbe4d5523a6e37ae16726b6
Harish Unnikrishnan <46891611+HTEN1998@users.noreply.github.com>
2019-12-23 21:36:39 +0530

    update Tkinter camera code

--- TkinterCam.py
+++ TkinterCam.py
@@ -6,7 +6,7 @@ import os
 from time import sleep
 import keyboard as raw_key
 from pynput import keyboard as py_key
-
+#from CameraMotion import CameraMotion
 
 class RcModeController:
     window = ""
@@ -26,6 +26,7 @@ class RcModeController:
     MoveBackward=""
     TurnLeft=""
     TurnRight=""
+    CamMotion = ""
 
 
     def __init__(self):
@@ -34,20 +35,25 @@ class RcModeController:
             self.window.geometry("620x580")
             self.window.resizable(0, 0)
             self.window.config(bg="black")
-            w_height, w_width = 620, 620
+            w_height, w_width = 640, 490
             self.window.geometry(f"{w_height}x{w_width}")
             width = self.window.winfo_screenwidth()
             height = self.window.winfo_screenheight()
             print(f"width x height -> {width}x{height}")
-            self.window.geometry(f"+{abs(w_width // 2 - width // 2)}+{abs(w_height // 2 - height // 2)}")
+            self.window.geometry(f"+{abs(0)}+{abs(0)}")
             self.window.overrideredirect(1)
             self.width = 620  # self.window.winfo_screenwidth()
             self.height = 620  # self.window.winfo_screenheight()
             print(f"width x height -> {360}x{360}")
+            self.window.focus()
+            #self.CamMotion = CameraMotion()
+            #self.CamMotion.ResetCameraPosition()
+            
+            
             if not path.exists("./SnapShots"):
                 os.mkdir("./SnapShots")
-            else:
-                self.ReadPresentImages()
+            #else:
+                #self.ReadPresentImages()
         except Exception as e:
             print(f"Exception in <Function> __init__ - <Class> RcModeController - <File> TkinterCam.py --> {e}")
             pass
@@ -64,21 +70,22 @@ class RcModeController:
         f_g = "blue"
         border_w=3
         if str(format(key)) == "'w'":
-            self.CamUp.config(bg=b_g, fg=f_g,width = 6 , borderwidth=border_w)
+            self.CamUp.config(bg=b_g, fg=f_g,width = 4 , borderwidth=border_w)
         if str(format(key)) == "'s'":
-            self.CamDown.config(bg=b_g, fg=f_g,width = 6, borderwidth=border_w)
+            self.CamDown.config(bg=b_g, fg=f_g,width = 4, borderwidth=border_w)
         if str(format(key)) == "'a'":
-            self.CamLeft.config(bg=b_g, fg=f_g,width = 6, borderwidth=border_w)
+            self.CamLeft.config(bg=b_g, fg=f_g,width = 4, borderwidth=border_w)
         if str(format(key)) == "'d'":
-            self.CamRight.config(bg=b_g, fg=f_g,width = 6, borderwidth=border_w)
+            self.CamRight.config(bg=b_g, fg=f_g,width = 4, borderwidth=border_w)
         if str(format(key)) == "Key.up":
-            self.MoveForward.config(bg=b_g, fg=f_g,width = 6 , borderwidth=border_w)
+            self.MoveForward.config(bg=b_g, fg=f_g,width = 4 , borderwidth=border_w)
         if str(format(key)) == "Key.down":
-            self.MoveBackward.config(bg=b_g, fg=f_g,width = 6, borderwidth=border_w)
+            self.MoveBackward.config(bg=b_g, fg=f_g,width = 4, borderwidth=border_w)
         if str(format(key)) == "Key.left":
-            self.TurnLeft.config(bg=b_g, fg=f_g,width = 6, borderwidth=border_w)
+            self.TurnLeft.config(bg=b_g, fg=f_g,width = 4, borderwidth=border_w)
         if str(format(key)) == "Key.right":
-            self.TurnRight.config(bg=b_g, fg=f_g,width = 6, borderwidth=border_w)
+            self.TurnRight.config(bg=b_g, fg=f_g,width = 4, borderwidth=border_w)
+
         print(f"pressed -> {str(format(key))}")
 
     def on_release(self, key):
@@ -86,26 +93,26 @@ class RcModeController:
         f_g = "black"
         border_w=1
         if str(format(key)) == "'w'":
-            self.CamUp.config(bg=b_g, fg=f_g,width = 5+1, borderwidth=border_w)
+            self.CamUp.config(bg=b_g, fg=f_g,width = 4, borderwidth=border_w)
         if str(format(key)) == "'s'":
-            self.CamDown.config(bg=b_g, fg=f_g,width = 5+1, borderwidth=border_w)
+            self.CamDown.config(bg=b_g, fg=f_g,width = 4, borderwidth=border_w)
         if str(format(key)) == "'a'":
-            self.CamLeft.config(bg=b_g, fg=f_g,width = 5+1, borderwidth=border_w)
+            self.CamLeft.config(bg=b_g, fg=f_g,width = 4, borderwidth=border_w)
         if str(format(key)) == "'d'":
-            self.CamRight.config(bg=b_g, fg=f_g,width = 5+1, borderwidth=border_w)
+            self.CamRight.config(bg=b_g, fg=f_g,width = 4, borderwidth=border_w)
         if str(format(key)) == "Key.up":
-            self.MoveForward.config(bg=b_g, fg=f_g,width = 6 , borderwidth=border_w)
+            self.MoveForward.config(bg=b_g, fg=f_g,width = 4 , borderwidth=border_w)
         if str(format(key)) == "Key.down":
-            self.MoveBackward.config(bg=b_g, fg=f_g,width = 6, borderwidth=border_w)
+            self.MoveBackward.config(bg=b_g, fg=f_g,width = 4, borderwidth=border_w)
         if str(format(key)) == "Key.left":
-            self.TurnLeft.config(bg=b_g, fg=f_g,width = 6, borderwidth=border_w)
+            self.TurnLeft.config(bg=b_g, fg=f_g,width = 4, borderwidth=border_w)
         if str(format(key)) == "Key.right":
-            self.TurnRight.config(bg=b_g, fg=f_g,width = 6, borderwidth=border_w)
+            self.TurnRight.config(bg=b_g, fg=f_g,width = 4, borderwidth=border_w)
 
     def StreamScreen(self):
         try:
 
-            self.vid = cv2.VideoCapture(0)
+            self.vid = ""#cv2.VideoCapture(0)
             print("Opencv init ..")
             listerner = py_key.Listener(
                 on_press=self.on_press,
@@ -117,20 +124,20 @@ class RcModeController:
             Label(self.window, font=("Courier bold", 20), text="VISIOR'S On Board Camera Stream", width="1080",
                   fg="#2ade2a",
                   bg="black").pack(side="top")
-            Button(self.window, font=("Courier bold", 15), text="Exit", command=self.window.destroy, width=5, bg="red",
+            Button(self.window, font=("Courier bold", 10), text="Exit", command=self.window.destroy, width=4, bg="red",
                    fg="white",
-                   borderwidth=0).place(relx=0.9, rely=0.0)
-            Button(self.window, font=("Courier bold", 15), text="Snapshot", command=self.SaveFrame, width=45,
+                   borderwidth=0).place(relx=0.91, rely=0.0)
+            Button(self.window, font=("Courier bold", 7), text="Snapshot", command=self.SaveFrame, width=108,
                    bg="#2ade2a",
                    fg="black",
-                   borderwidth=1).place(relx=0.1, rely=0.6)
+                   borderwidth=0).place(relx=0.0, rely=0.6)
 
             self.CreateCameraButtons()
             self.CreateMovementButton()
 
-            self.canvas = Label(self.window, width=self.width, height=self.height // 2, bg="black")
+            self.canvas = Label(self.window, width = 88, height=15, bg="red")
             self.canvas.pack()
-            self.update()
+            #self.update()
 
             self.window.mainloop()
         except Exception as e:
@@ -176,58 +183,52 @@ class RcModeController:
 
 
     def CreateCameraButtons(self):
-        self.CamUp = Button(self.window, font=("Courier bold", 11), text="Up(W)", width=6,
+        self.CamUp = Button(self.window, font=("Courier bold", 11), text="Up(W)", width=4,
                             bg="#2ade2a",
                             fg="black",
-                            borderwidth=1)
-        self.CamUp.place(relx=0.21, rely=0.74)
-        self.CamRight = Button(self.window, font=("Courier bold", 11), text="Right(D)", width=6,
+                            borderwidth=2,highlightthickness=0)
+        self.CamUp.place(relx=0.21, rely=0.72)
+        self.CamRight = Button(self.window, font=("Courier bold", 11), text="Right(D)", width=4,
                                bg="#2ade2a",
                                fg="black",
-                               borderwidth=1)
+                               borderwidth=2,highlightthickness=0)
         self.CamRight.place(relx=0.32, rely=0.8)
-        self.CamDown = Button(self.window, font=("Courier bold", 11), text="Down(S)", width=6,
+        self.CamDown = Button(self.window, font=("Courier bold", 11), text="Down(S)", width=4,
                               bg="#2ade2a",
                               fg="black",
-                              borderwidth=1)
+                              borderwidth=2,highlightthickness=0)
         self.CamDown.place(relx=0.21, rely=0.8)
-        self.CamLeft = Button(self.window, font=("Courier bold", 11), text="Left(A)", width=6,
+        self.CamLeft = Button(self.window, font=("Courier bold", 11), text="Left(A)", width=4,
                               bg="#2ade2a",
                               fg="black",
-                              borderwidth=1)
+                              borderwidth=2,highlightthickness=0)
         self.CamLeft.place(relx=0.1, rely=0.8)
 
         Label(self.window, font=("Courier bold", 12), text="Camera Control", width=len(" Camera Control "),
               fg="white",
-              bg="black").place(relx=0.139, rely=0.86)
+              bg="black").place(relx=0.139, rely=0.89)
 
     def CreateMovementButton(self):
-        self.MoveForward = Button(self.window, font=("Courier bold", 11), text="Front(^)", width=6,
+        self.MoveForward = Button(self.window, font=("Courier bold", 11), text="Front(^)", width=4,
                             bg="#2ade2a",
                             fg="black",
-                            borderwidth=1)
-        self.MoveForward.place(relx=0.7, rely=0.74)
-        self.MoveBackward = Button(self.window, font=("Courier bold", 11), text="Back(!)", width=6,
+                            borderwidth=2,highlightthickness=0)
+        self.MoveForward.place(relx=0.7, rely=0.72)
+        self.MoveBackward = Button(self.window, font=("Courier bold", 11), text="Back(!)", width=4,
                                   bg="#2ade2a",
                                   fg="black",
-                                  borderwidth=1)
+                                  borderwidth=2,highlightthickness=0)
         self.MoveBackward.place(relx=0.7, rely=0.8)
-        self.TurnLeft = Button(self.window, font=("Courier bold", 11), text="Left(<)", width=6,
+        self.TurnLeft = Button(self.window, font=("Courier bold", 11), text="Left(<)", width=4,
                                    bg="#2ade2a",
                                    fg="black",
-                                   borderwidth=1)
+                                   borderwidth=2,highlightthickness=0)
         self.TurnLeft.place(relx=0.59, rely=0.8)
-        self.TurnRight = Button(self.window, font=("Courier bold", 11), text="Right(>)", width=6,
+        self.TurnRight = Button(self.window, font=("Courier bold", 11), text="Right(>)", width=4,
                                    bg="#2ade2a",
                                    fg="black",
-                                   borderwidth=1)
+                                   borderwidth=2,highlightthickness=0)
         self.TurnRight.place(relx=0.81, rely=0.8)
         Label(self.window, font=("Courier bold", 12), text="Motion Control", width=len(" Camera Control "),
               fg="white",
-              bg="black").place(relx=0.629, rely=0.86)
-
-
-
-
-# if __name__ == "__main__":
-#    TkinterCam()
+              bg="black").place(relx=0.63, rely=0.89)
