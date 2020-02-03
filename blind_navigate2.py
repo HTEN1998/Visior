9efebd928cfdfc3b40d8e078c6b46ad0bd999b63
Harish Unnikrishnan <46891611+HTEN1998@users.noreply.github.com>
2020-02-03 22:44:27 +0530

    added backtracking feature

--- blind_navigate2.py
+++ blind_navigate2.py
@@ -1,43 +1,50 @@
-# navigation by distance calulation
+#inculded backtracking feature
 
 class Simlutation():
 
 	max_extend=0
 	current_row, current_col = 0,0
 	goal_node_row,goal_node_col = 0,0
+	blocks=set()
 	history=[(-1,-1)]
 
-	def check_up(self,block_row,block_col):
-		if(self.current_row-1<0 or (block_row==self.current_row-1 and block_col==self.current_col)):
+	def check_up(self):
+		if(self.current_row-1<0 or (self.current_row-1,self.current_col) in self.blocks):
 			return -1
 		else:
-			if (self.current_row-1==self.history[len(self.history)-1][0] and self.current_col==self.history[len(self.history)-1][1]):
+			if(self.current_row-1,self.current_col) == self.history[len(self.history)-2]:
 				return -1
 			return abs(self.current_row-1-self.goal_node_row)+abs(self.current_col-self.goal_node_col)
 
-	def check_down(self,block_row,block_col):
-		if(self.current_row+1>self.max_extend or (block_row==self.current_row+1 and block_col==self.current_col)):
+	def check_down(self):
+		if(self.current_row+1>self.max_extend or (self.current_row+1,self.current_col) in self.blocks):
 			return -1
 		else:
-			if (self.current_row+1==self.history[len(self.history)-1][0] and self.current_col==self.history[len(self.history)-1][1]):
+			if(self.current_row+1,self.current_col) == self.history[len(self.history)-2]:
 				return -1
 			return abs(self.current_row+1-self.goal_node_row)+abs(self.current_col-self.goal_node_col)
 
-	def check_right(self,block_row,block_col):
-		if(self.current_col+1>self.max_extend or (block_row==self.current_row and block_col==self.current_col+1)):
+	def check_right(self):
+		if(self.current_col+1>self.max_extend or (self.current_row,self.current_col+1) in self.blocks):
 			return -1
 		else:
-			if (self.current_row==self.history[len(self.history)-1][0] and self.current_col+1==self.history[len(self.history)-1][1]):
+			if(self.current_row,self.current_col+1) == self.history[len(self.history)-2]:
 				return -1
 			return abs(self.current_row-self.goal_node_row)+abs(self.current_col+1-self.goal_node_col)
 
-	def check_left(self,block_row,block_col):
-		if(self.current_col-1<0 or (block_row==self.current_row and block_col==self.current_col-1)):
+	def check_left(self):
+		if(self.current_col-1<0 or (self.current_row,self.current_col-1) in self.blocks):
 			return -1
 		else:
-			if (self.current_row==self.history[len(self.history)-1][0] and self.current_col-1==self.history[len(self.history)-1][1]):
+			if(self.current_row,self.current_col-1) == self.history[len(self.history)-2]:
 				return -1
 			return abs(self.current_row-self.goal_node_row)+abs(self.current_col-1-self.goal_node_col)
+
+	def backtracker(self):
+		temp_tuple = self.history.pop()
+		self.blocks.add(temp_tuple)
+		temp_tuple = self.history.pop()
+		self.navigate(temp_tuple[0],temp_tuple[1])
 
 	def navigate(self,start_row,start_col):
 		directions=[]
@@ -45,22 +52,24 @@ class Simlutation():
 		self.current_col=start_col
 		print("Now At-> %d %d"%(self.current_row,self.current_col))
 
-		if (self.current_row==self.goal_node_row and self.current_col==self.goal_node_col):
+		self.history.append((self.current_row,self.current_col))	#appending previous values
+		
+		if (self.current_row==self.goal_node_row and self.current_col==self.goal_node_col):	#exit point
 			return
 
-		block=input("Does Object present [y/n]?")
-		if block=='n':
-			block_row, block_col = -1,-1 # no block added
-		elif block=='y':
+		block=int(input("Does Object present [0/1]?"))
+		if block==1:
 			block_row, block_col = map(int,input("Mention: row col-> ").split(" "))
+			tup = (block_row,block_col)
+			self.blocks.add(tup)
+			del tup
 
-		directions.append(self.check_up(block_row,block_col))
-		directions.append(self.check_down(block_row,block_col))
-		directions.append(self.check_right(block_row,block_col))
-		directions.append(self.check_left(block_row,block_col))
+		directions.append(self.check_up())
+		directions.append(self.check_down())
+		directions.append(self.check_right())
+		directions.append(self.check_left())
 		print("direction nav-> ",directions)
 		min_index=self.get_min_distance(directions)
-		#print("min_index-> ",min_index)
 
 		if (min_index==0):#up
 			self.get_decision(self.current_row-1,self.current_col)
@@ -71,17 +80,13 @@ class Simlutation():
 		elif (min_index==3):#left
 			self.get_decision(self.current_row,self.current_col-1)
 		elif (min_index==-1):#no direction found
-			x = self.history.pop()
-			print("prev_row = %d & prev_col = %d"%(x[0], x[1]))
+			self.backtracker()
 
-	def setup_world(self,sizeof_world):
-		self.max_extend=sizeof_world
+	def setup_world(self):
+		self.max_extend=int(input('Enter matrix size? '))
 		self.goal_node_row,self.goal_node_col=map(int,input("Goal Rows Col-> ").split(" "))
 
 	def get_decision(self,new_row,new_col):
-
-		self.history.append((self.current_row,self.current_col))	#appending previous values
-		
 		self.current_row=new_row
 		self.current_col=new_col
 
@@ -99,7 +104,7 @@ class Simlutation():
 
 
 s=Simlutation()
-s.setup_world(4)
+s.setup_world()
 s.navigate(s.current_row,s.current_col)
 print("----END----")
 print("Path",s.history)
