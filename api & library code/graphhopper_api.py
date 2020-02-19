d612213b4d67714330a2e6a2e40920467d98f948
Harish Unnikrishnan <46891611+HTEN1998@users.noreply.github.com>
2020-02-19 22:58:56 +0530

    Update graphhopper_api.py

--- api & library code/graphhopper_api.py
+++ api & library code/graphhopper_api.py
@@ -1,7 +1,7 @@
 import requests
 from pprint import pprint
 
-r = requests.get("https://graphhopper.com/api/1/route?point=19.185206, 72.975723&point=19.179911, 72.980190&vehicle=foot&locale=en&calc_points=true&key=bb5794b7-0b67-401c-834c-81aee1a1302d")
+r = requests.get("https://graphhopper.com/api/1/route?point=19.185206, 72.975723&point=19.179911, 72.980190&vehicle=foot&locale=en&calc_points=true&key=api_key")
 
 pprint(r.json())
 
