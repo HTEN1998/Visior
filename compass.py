9e8c402338065c6808c715e1d9e8d560b2b8796d
Harish Unnikrishnan <46891611+HTEN1998@users.noreply.github.com>
2020-02-19 23:10:14 +0530

    large and short range navigation

--- compass.py
+++ compass.py
@@ -19,34 +19,63 @@ class auto_traction():
 			self.compass_angle=0
 
 	def compass(self,input_direction):
-		if input_direction=="front":
+		
+		if input_direction=="left":
+			print("------------>  turn left")
+			go_forward()
+		elif input_direction=="right":
+			print("------------>  turn right")
 			self.go_forward()
-			self.left_sensor=input("left block? ")
-			self.right_sensor=input("right block? ")
-			if(self.left_sensor=='0' and self.right_sensor=='0'):	#no block detected in left and right
+		elif input_direction=="front":
+			self.go_forward()
 
-			elif(self.left_sensor=='1' and self.right_sensor=='0'): #no block detected on left 
-				print("------------>  move left")
+		self.left_sensor=input("left block? ")
+		self.right_sensor=input("right block? ")
+		if(self.left_sensor=='0' and self.right_sensor=='0'):	#no block detected in left and right
+			temp_left_angle=self.compass_angle+90
+			temp_right_angle=self.compass_angle-90
+
+			# covert 360 degree to zero
+			if(temp_left_angle==360 or temp_left_angle==-360): temp_left_angle=0
+			if(temp_right_angle==360 or temp_right_angle==-360): temp_right_angle=0
+
+			left_list=[abs(-360-temp_left_angle),abs(360-temp_left_angle),abs(0-temp_left_angle)]
+			right_list=[abs(-360-temp_right_angle),abs(360-temp_right_angle),abs(0-temp_right_angle)]
+			if (min(left_list)<min(right_list)):
+				print("------------>  turn left")
 				self.compass_angle+=90
 				self.reset_compass_angle()
 				self.go_forward()
 
-			elif(self.left_sensor=='0' and self.right_sensor=='1'): #no block detected on right 
+			elif(min(left_list)>min(right_list)):
+				print("------------>  move right")
+				self.compass_angle-=90
+				self.reset_compass_angle()
+				self.go_forward()
+			else:
 				print("------------>  move right")
 				self.compass_angle-=90
 				self.reset_compass_angle()
 				self.go_forward()
 
-			elif(self.left_sensor=='1' and self.right_sensor=='1'):  #dead end conditon
-				print("------------>  move back")
-				
 
-		elif input_direction=="left":
-			print("------------>  turn left")
-			self.go_forward()
-		elif input_direction=="right":
-			print("------------>  turn right")
-			self.go_forward()
+		elif(self.left_sensor=='0' and self.right_sensor=='1'): #no block detected on left 
+				print("------------>  move left")
+				self.compass_angle+=90
+				self.reset_compass_angle()
+				self.go_forward()
 
+		elif(self.left_sensor=='1' and self.right_sensor=='0'): #no block detected on right 
+				print("------------>  move right")
+				self.compass_angle-=90
+				self.reset_compass_angle()
+				self.go_forward()
+
+		elif(self.left_sensor=='1' and self.right_sensor=='1'):  #dead end conditon
+			print("------------>  move back")
+		
+		
 nv=auto_traction()
-nv.compass()
+for i in range(5):nv.compass("front")
+# nv.compass("left")
+# nv.compass("right")
